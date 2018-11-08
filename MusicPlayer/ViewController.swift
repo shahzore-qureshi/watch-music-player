402f25f1db2f9cdd91a39493e043ca77f09e0a50
Shahzore Qureshi <shahzore.qureshi@gmail.com>
2018-11-08 17:08:17 -0500

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by shahzore.qureshi on 11/8/18.
+//  Copyright © 2018 shahzore.qureshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
