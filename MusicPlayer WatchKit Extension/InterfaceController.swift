85a16ee5f6a3958a76818444d7203ec3b14cf4b3
Shahzore Qureshi <shahzore.qureshi@gmail.com>
2018-11-12 13:11:45 -0500

    Sounds now play through watch.

--- MusicPlayer WatchKit Extension/InterfaceController.swift
+++ MusicPlayer WatchKit Extension/InterfaceController.swift
@@ -8,14 +8,40 @@
 
 import WatchKit
 import Foundation
-
+import AVFoundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var button: WKInterfaceButton!
+    var buttonLabel: String = "Ring!"
+    
+    var audioSession: AVAudioSession? = nil
+    var audioPlayer: AVAudioPlayer? = nil
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession?.setCategory(
+                AVAudioSession.Category.playback,
+                mode: .default,
+                policy: .default,
+                options: []
+            )
+        } catch {
+            fatalError("Unable to set up audio session")
+        }
+        
+        do {
+            let songPath = Bundle.main.path(forResource: "chicken", ofType: "wav")!
+            print("songpath: \(songPath)")
+            let songURL = URL(fileURLWithPath: songPath)
+            audioPlayer = try AVAudioPlayer(contentsOf: songURL)
+        } catch {
+            fatalError("Unable to set up player")
+        }
     }
     
     override func willActivate() {
@@ -26,6 +52,26 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        button.setTitle("Ring!")
     }
-
+    
+    @IBAction func onButtonClick() {
+        if(buttonLabel == "Ring!") {
+            buttonLabel = "Ringing..."
+        } else {
+            buttonLabel = "Ring!"
+        }
+        button.setTitle(buttonLabel)
+        
+        audioSession?.activate(options: []) { (success, error) in
+            guard error == nil else {
+                print("*** An error occurred: \(error!.localizedDescription) ***")
+                // Handle the error here.
+                return
+            }
+            
+            // Play the audio file.
+            self.audioPlayer?.play()
+        }
+    }
 }
